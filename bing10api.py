5c546d7fdae087326209327bf4385c6a98107889
theurs <theurs@gmail.com>
2025-02-05 14:16:06 +1000

    fix

--- bing10api.py
+++ bing10api.py
@@ -64,15 +64,15 @@ def bing(j: Dict[str, Any], iterations=1) -> Dict[str, Any]:
             COOKIE_INITIALIZED = True
 
 
-        # принудительно сменить куки после определенного количества запросов
-        if REQUESTS_BEFORE_ROTATE_COOKIE > MAX_REQUESTS_BEFORE_ROTATE_COOKIE:
-            rotate_cookie.rotate_cookie()
-            REQUESTS_BEFORE_ROTATE_COOKIE = 0
-            COOKIE_FAIL = 0
-            COOKIE_FAIL_FOR_TERMINATE = 0
-            my_log.log2(f'rotate_cookie: after {MAX_REQUESTS_BEFORE_ROTATE_COOKIE} requests')
-        else:
-            REQUESTS_BEFORE_ROTATE_COOKIE += 1
+        # # принудительно сменить куки после определенного количества запросов
+        # if REQUESTS_BEFORE_ROTATE_COOKIE > MAX_REQUESTS_BEFORE_ROTATE_COOKIE:
+        #     rotate_cookie.rotate_cookie()
+        #     REQUESTS_BEFORE_ROTATE_COOKIE = 0
+        #     COOKIE_FAIL = 0
+        #     COOKIE_FAIL_FOR_TERMINATE = 0
+        #     my_log.log2(f'rotate_cookie: after {MAX_REQUESTS_BEFORE_ROTATE_COOKIE} requests')
+        # else:
+        #     REQUESTS_BEFORE_ROTATE_COOKIE += 1
 
 
         # Get JSON data from the request
