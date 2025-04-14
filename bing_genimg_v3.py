0809e488bed013fa6f2c62b9a943c7d2a319a70e
theurs <theurs@gmail.com>
2025-04-14 22:32:59 +1000

    fix

--- bing_genimg_v3.py
+++ bing_genimg_v3.py
@@ -20,7 +20,7 @@ class BingBrush:
         self,
         cookie,
         verbose=False,
-        max_wait_time=60,
+        max_wait_time=90,
     ):
         self.max_wait_time = max_wait_time
         self.verbose = verbose
